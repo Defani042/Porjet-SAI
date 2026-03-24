5539c74c781d1a51835b9cd2d535cfc29ba67f26
Gault115 <gaultierpeyrard2003@gmail.com>
2026-03-24 09:30:34 +0100

    Feat : gestion des déplacement et de la direction

--- src/main.c
+++ src/main.c
@@ -6,18 +6,7 @@
 
 int main(int argc, char *argv[]){
     glutInit(&argc, argv);                
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-    glutInitWindowPosition(50, 50);  
-    glutCreateWindow("Projet SAI");
-    glutFullScreen();
-    glEnable(GL_DEPTH_TEST);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glutDisplayFunc(affichage);   
-    glutIdleFunc(animer);
-    glutKeyboardFunc(touche_pressee);
-    glutKeyboardUpFunc(touche_relachee);
-    glutPassiveMotionFunc(mouvement_souris);
-    glutMotionFunc(mouvement_souris);
+    initialisation();
     glutMainLoop();
     exit(EXIT_SUCCESS);
 }
