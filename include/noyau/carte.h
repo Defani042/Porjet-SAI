0bdabb74ef5a89be6513e1a7d1e22b254194c9b6
Defani <adrien.defay@outlook.com>
2026-03-28 17:04:17 +0100

    Ajout: 	+ ajout d'un module pour gestion de deplacement 	+ ajout d'un module de gestion des collision 	+ ajout d'une génération aléatoire des ennemie (NON OK) 	+ Ajout dun tableau d'ennemi 	+ init des tableau 	+ ajout de l'affichage des ennemi Modification: 	* modification des fonction dans carte pour mettre dans deplacement ou collision 	* modification de l'interface

--- include/noyau/carte.h
+++ include/noyau/carte.h
@@ -28,31 +28,8 @@ void liberer_carte(carte c);
 
 carte creer_carte_vide();
 
-
-int collision_hitbox(
-    double x1, double y1, double z1,
-    double w1, double h1, double l1,
-    double x2, double y2, double z2,
-    double w2, double h2, double l2
-);
-
-int collision_objet(
-    double x1, double y1, double z1,
-    double w1, double h1, double l1,
-    double x2, double y2, double z2,
-    double w2, double h2, double l2
-);
-
-objet detecter_collision_joueur(carte c);
-
-void deplacer_joueur(carte c, double dirx, double diry, double dirz);
-
 int game_over(carte c);
 
 int objet_visible(joueur j, objet o);
-
-void deplacer_ennemi_vers_joueur(carte c, ennemi e, joueur joueur);
-
-void avencer_vague_ennemi(carte c);
 
 #endif /*_CARTE_H_*/
