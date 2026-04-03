424df94874459c97318cc03f2323f43a5c6503c0
Defani <adrien.defay@outlook.com>
2026-04-03 17:58:36 +0200

    Ajout: 	+ Ajout du module pour la gestion de la difficulté

---
+++ include/noyau/difficulte.h
@@ -0,0 +1,11 @@
+#ifndef _DIFFICULTE_H_
+#define _DIFFICULTE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "moteur/interface.h"
+
+
+#endif /*_DIFFICULTE_H_*/
---
+++ src/noyau/difficulte.c
@@ -0,0 +1,7 @@
+#ifndef _DIFFICULTE_H_
+#define _DIFFICULTE_H_
+
+#include "noyau/difficulte.h"
+
+
+#endif /*_DIFFICULTE_H_*/
